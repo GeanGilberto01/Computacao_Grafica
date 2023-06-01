91d7e66b152c02a8c46cd01b17e2aac06a235a65
Gean Gilberto Coimbra <43455735+GeanGilberto01@users.noreply.github.com>
2023-05-31 21:47:14 -0300

    Update Exercicio_7.cpp

--- Exercicio_7/Exercicio_7.cpp
+++ Exercicio_7/Exercicio_7.cpp
@@ -9,8 +9,8 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h);
 void Desenha(void);
 void Quadrado(void);
 void GerenciaMouse(int button, int state, int x, int y);
-void MousePosition(int x, int y);
-void MousePassiveMotion(int x, int y);
+void MousePressionado(int x, int y);
+void MouseSolto(int x, int y);
 
 bool pressionado = false;
 int iniX, iniY;
@@ -21,47 +21,47 @@ float QuadY = 0.0f;
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);                                // Inicializa a GLUT
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);          // Modo de operaÁ„o da GLUT
-    glutInitWindowPosition(350, 100);                     // posiciona a janela na tela. A referÍncia È o canto superior esquerdo
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);          // Modo de opera√ß√£o da GLUT
+    glutInitWindowPosition(350, 100);                     // posiciona a janela na tela. A refer√™ncia √© o canto superior esquerdo
     glutInitWindowSize(640, 480);                         // tamanho em pixels da janela
-    glutCreateWindow("Exercicio 7");                      // Cria a janela passando como argumento o tÌtulo da mesma
+    glutCreateWindow("Exercicio 7");                      // Cria a janela passando como argumento o t√≠tulo da mesma
     Inicializa();
     glutReshapeFunc(AlteraTamanhoJanela);
-    glutDisplayFunc(Desenha);                              // Registra funÁ„o de redesenho
+    glutDisplayFunc(Desenha);                              // Registra fun√ß√£o de redesenho
     glutMouseFunc(GerenciaMouse);                          // Gerencia Click do Mouse
-    glutMotionFunc(MousePosition);                         // Verifica a posicao do mouse quando o bot„o esquerdo est· pressionado
-    glutPassiveMotionFunc(MousePassiveMotion);             // Verifica a posicao do mouse quando nenhum bot„o est· pressionado
-    glutMainLoop();                                        // Inicia o processamento e aguarda interaÁıes do usu·rio
+    glutMotionFunc(MousePressionado);                         // Verifica a posicao do mouse quando o bot√£o esquerdo est√° pressionado
+    glutPassiveMotionFunc(MouseSolto);             // Verifica a posicao do mouse quando nenhum bot√£o est√° pressionado
+    glutMainLoop();                                        // Inicia o processamento e aguarda intera√ß√µes do usu√°rio
     return 0;
 }
 
-// Inicializa par‚metros de rendering
+// Inicializa par√¢metros de rendering
 void Inicializa(void)
 {
-    // Define a cor de fundo da janela de visualizaÁ„o como preta
+    // Define a cor de fundo da janela de visualiza√ß√£o como preta
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glutIdleFunc(Desenha);
 }
 
-// FunÁ„o callback chamada quando o tamanho da janela È alterado
+// Fun√ß√£o callback chamada quando o tamanho da janela √© alterado
 void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 {
     // Evita a divisao por zero
     if (h == 0)
         h = 1;
 
-    // Especifica as dimensıes da Viewport
+    // Especifica as dimens√µes da Viewport
     glViewport(0, 0, w, h);
 
     // Inicializa o sistema de coordenadas
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    // Estabelece a janela de seleÁ„o (left, right, bottom, top)
+    // Estabelece a janela de sele√ß√£o (left, right, bottom, top)
     gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
 }
 
-// FunÁ„o de redesenho da janela de visualizaÁ„o
+// Fun√ß√£o de redesenho da janela de visualiza√ß√£o
 void Desenha(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -77,23 +77,23 @@ void Desenha(void)
     glutSwapBuffers();
 }
 
-// FunÁ„o respons·vel pelo quadrado
+// Fun√ß√£o respons√°vel pelo quadrado
 void Quadrado(void)
 {
     glColor3f(0.0, 0.0, 1.0); // Azul
-    glBegin(GL_QUADS);        // define o inÌcio de um polÌgono com quatro vÈrtices
-    glVertex2f(-0.25, -0.25);   // define o primeiro vÈrtice
-    glVertex2f( 0.25, -0.25);   // define o segundo vÈrtice
-    glVertex2f( 0.25,  0.25);   // define o terceiro vÈrtice
-    glVertex2f(-0.25,  0.25);   // define o quarto vÈrtice
-    glEnd();                  // define o fim do polÌgono
+    glBegin(GL_QUADS);        // define o in√≠cio de um pol√≠gono com quatro v√©rtices
+    glVertex2f(-0.25, -0.25);   // define o primeiro v√©rtice
+    glVertex2f( 0.25, -0.25);   // define o segundo v√©rtice
+    glVertex2f( 0.25,  0.25);   // define o terceiro v√©rtice
+    glVertex2f(-0.25,  0.25);   // define o quarto v√©rtice
+    glEnd();                  // define o fim do pol√≠gono
 }
 
 void GerenciaMouse(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) // Botao esquerdo do mouse pressionado
     {
-        // Normalizando a posiÁ„o do mouse para variar de -1 a 1
+        // Normalizando a posi√ß√£o do mouse para variar de -1 a 1
         float NormalizaX = (x - 320.0f) / 320.0f;
         float NormalizaY = (240.0f - y) / 240.0f;
 
@@ -119,7 +119,7 @@ void GerenciaMouse(int button, int state, int x, int y)
     }
 }
 
-void MousePosition(int x, int y)
+void MousePressionado(int x, int y)
 {
     if (pressionado)
     {
@@ -138,8 +138,7 @@ void MousePosition(int x, int y)
     }
 }
 
-void MousePassiveMotion(int x, int y)
+void MouseSolto(int x, int y)
 {
     printf("Mouse ANDANDO solto na janela. Posicao: (%d, %d)\n", x, y);
 }
-
